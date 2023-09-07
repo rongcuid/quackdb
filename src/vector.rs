9739de8d5433411e4423ad157dd7e64a4c1d4162
Rongcui Dong <rongcuid@outlook.com>
2023-09-07 15:21:42 -0400

    WIP

--- src/vector.rs
+++ src/vector.rs
@@ -17,8 +17,12 @@ impl Vector {
     pub fn column_type(&self) -> Option<LogicalType> {
         unsafe { LogicalType::from_raw(ffi::duckdb_vector_get_column_type(self.handle.0)) }
     }
-    // pub fn data(&self)
-    // pub fn get_validity
+    pub fn data(&self) {
+        todo!()
+    }
+    pub fn validity(&self) {
+        todo!()
+    }
     pub fn ensure_validity_writable(&self) {
         unsafe {
             ffi::duckdb_vector_ensure_validity_writable(self.handle.0);
