6d456f35b4dca0572452f598a0357d938500f946
Rongcui Dong <rongcuid@outlook.com>
2023-09-08 10:06:46 -0400

    Be pedantic and use u64

--- src/data_chunks.rs
+++ src/data_chunks.rs
@@ -13,7 +13,7 @@ pub struct DataChunk {
 struct DataChunkHandle(ffi::duckdb_data_chunk);
 
 impl DataChunk {
-    pub fn new(ty: RawType, column_count: usize) -> Arc<Self> {
+    pub fn new(ty: RawType, column_count: u64) -> Arc<Self> {
         unimplemented!()
     }
     pub fn reset(&self) {
@@ -21,21 +21,20 @@ impl DataChunk {
             ffi::duckdb_data_chunk_reset(self.handle.lock().unwrap().0);
         }
     }
-    pub fn column_count(&self) -> usize {
-        unsafe { ffi::duckdb_data_chunk_get_column_count(self.handle.lock().unwrap().0) as usize }
+    pub fn column_count(&self) -> u64 {
+        unsafe { ffi::duckdb_data_chunk_get_column_count(self.handle.lock().unwrap().0) }
     }
-    pub fn vector(&self, col_idx: usize) -> Arc<Vector> {
+    pub fn vector(&self, col_idx: u64) -> Arc<Vector> {
         unsafe {
-            let v =
-                ffi::duckdb_data_chunk_get_vector(self.handle.lock().unwrap().0, col_idx as u64);
+            let v = ffi::duckdb_data_chunk_get_vector(self.handle.lock().unwrap().0, col_idx);
             Vector::from_raw(v, None)
         }
     }
-    pub fn size(&self) -> usize {
-        unsafe { ffi::duckdb_data_chunk_get_size(self.handle.lock().unwrap().0) as usize }
+    pub fn size(&self) -> u64 {
+        unsafe { ffi::duckdb_data_chunk_get_size(self.handle.lock().unwrap().0) }
     }
-    pub fn set_size(&self, size: usize) {
-        unsafe { ffi::duckdb_data_chunk_set_size(self.handle.lock().unwrap().0, size as u64) }
+    pub fn set_size(&self, size: u64) {
+        unsafe { ffi::duckdb_data_chunk_set_size(self.handle.lock().unwrap().0, size) }
     }
 }
 
--- src/query.rs
+++ src/query.rs
@@ -27,17 +27,17 @@ impl QueryResult {
         })
     }
 
-    pub fn column_name(&self, col: usize) -> Option<String> {
+    pub fn column_name(&self, col: u64) -> Option<String> {
         unsafe {
             let p: *const c_char =
-                ffi::duckdb_column_name(self.handle.lock().unwrap().deref_mut(), col as u64);
+                ffi::duckdb_column_name(self.handle.lock().unwrap().deref_mut(), col);
             let nn = NonNull::new(p as *mut c_char)?;
             let cstr = CStr::from_ptr(nn.as_ptr());
             Some(cstr.to_string_lossy().to_string())
         }
     }
 
-    pub fn column_type(&self, col: usize) -> Option<LogicalType> {
+    pub fn column_type(&self, col: u64) -> Option<LogicalType> {
         unsafe {
             LogicalType::from_raw(ffi::duckdb_column_logical_type(
                 self.handle.lock().unwrap().deref_mut(),
@@ -46,14 +46,14 @@ impl QueryResult {
         }
     }
 
-    pub fn column_count(&self) -> usize {
-        unsafe { ffi::duckdb_column_count(self.handle.lock().unwrap().deref_mut()) as usize }
+    pub fn column_count(&self) -> u64 {
+        unsafe { ffi::duckdb_column_count(self.handle.lock().unwrap().deref_mut()) }
     }
-    pub fn row_count(&self) -> usize {
-        unsafe { ffi::duckdb_row_count(self.handle.lock().unwrap().deref_mut()) as usize }
+    pub fn row_count(&self) -> u64 {
+        unsafe { ffi::duckdb_row_count(self.handle.lock().unwrap().deref_mut()) }
     }
-    pub fn rows_changed(&self) -> usize {
-        unsafe { ffi::duckdb_rows_changed(self.handle.lock().unwrap().deref_mut()) as usize }
+    pub fn rows_changed(&self) -> u64 {
+        unsafe { ffi::duckdb_rows_changed(self.handle.lock().unwrap().deref_mut()) }
     }
 }
 
