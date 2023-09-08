bcf5ce5c94178e1dd6540c91ce80dce31eda1c0e
Rongcui Dong <rongcuid@outlook.com>
2023-09-08 13:09:00 -0400

    Stub

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 mod cutils;
 
+mod appender;
 mod config;
 mod connection;
 mod data_chunks;
@@ -8,7 +9,9 @@ mod error;
 mod ffi;
 mod logical_type;
 mod query;
+mod replacement_scan;
 mod statement;
+mod table_function;
 mod types;
 /// NOTE: Values API is not fully ported to C
 mod value;
