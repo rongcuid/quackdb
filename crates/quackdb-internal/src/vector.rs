70bcc389a8507c788fcbc7958cf3b5cbf5c08379
Rongcui Dong <rongcuid@outlook.com>
2023-09-09 11:16:28 -0400

    Vector and validity internals

--- crates/quackdb-internal/src/vector.rs
+++ crates/quackdb-internal/src/vector.rs
@@ -20,6 +20,8 @@ impl Deref for VectorHandle {
 }
 
 impl VectorHandle {
+    /// # Safety
+    /// Takes ownership.
     pub unsafe fn from_raw(raw: ffi::duckdb_vector) -> Arc<Self> {
         assert!(raw != std::ptr::null_mut());
         Arc::new(Self {
@@ -28,8 +30,8 @@ impl VectorHandle {
         })
     }
 
-    pub unsafe fn column_type(&self) -> LogicalTypeHandle {
-        LogicalTypeHandle::from_raw(ffi::duckdb_vector_get_column_type(self.handle))
+    pub fn column_type(&self) -> LogicalTypeHandle {
+        unsafe { LogicalTypeHandle::from_raw(ffi::duckdb_vector_get_column_type(self.handle)) }
     }
     pub fn data(&self) {
         todo!()
@@ -46,34 +48,55 @@ impl VectorHandle {
             ffi::duckdb_vector_ensure_validity_writable(self.handle);
         }
     }
+    /// # Safety
+    /// Ensure:
+    /// * This is string vector
+    /// * `index` is in range
+    /// * `str` is null-terminated
     pub unsafe fn assign_string_element(&self, index: u64, str: *const c_char) {
         ffi::duckdb_vector_assign_string_element(self.handle, index, str);
     }
-
+    /// # Safety
+    /// Ensure:
+    /// * This is string vector
+    /// * `index` is in range
+    /// * `str_len` does not cause out-of-bounds access
     pub unsafe fn assign_string_element_len(&self, index: u64, str: *const c_char, str_len: u64) {
         ffi::duckdb_vector_assign_string_element_len(self.handle, index, str, str_len);
     }
+    /// # Safety
+    /// This must be a list vector
     pub unsafe fn list_child(self: &Arc<Self>) -> Arc<Self> {
         Arc::new(Self {
             handle: ffi::duckdb_list_vector_get_child(self.handle),
             _parent: Some(self.clone()),
         })
     }
+    /// # Safety
+    /// This must be a list vector
     pub unsafe fn list_size(&self) -> u64 {
         ffi::duckdb_list_vector_get_size(self.handle)
     }
+    /// # Safety
+    /// * This must be a list vector
+    /// * `size` must be valid
     pub unsafe fn list_set_size(&self, size: u64) {
         let res = ffi::duckdb_list_vector_set_size(self.handle, size);
         if res == ffi::DuckDBError {
             unreachable!("Vector pointer is null");
         }
     }
+    /// # Safety
+    /// * This must be a list vector
+    /// * `required_capacity` must be valid
     pub unsafe fn list_reserve(&self, required_capacity: u64) {
         let res = ffi::duckdb_list_vector_reserve(self.handle, required_capacity);
         if res == ffi::DuckDBError {
             unreachable!("Vector pointer is null");
         }
     }
+    /// # Safety
+    /// This must be a struct vector
     pub unsafe fn struct_child(self: &Arc<Self>, index: u64) -> Arc<Self> {
         Arc::new(Self {
             handle: ffi::duckdb_struct_vector_get_child(self.handle, index),
@@ -91,12 +114,20 @@ impl Deref for ValidityHandle {
 }
 
 impl ValidityHandle {
+    /// # Safety
+    /// Takes ownership
     pub unsafe fn from_raw(raw: *mut u64) -> Self {
         Self(raw)
     }
+    /// # Safety
+    /// * Validity must be writable
+    /// * `row` must be in range
     pub unsafe fn row_is_valid(&self, row: u64) -> bool {
         unsafe { ffi::duckdb_validity_row_is_valid(self.0, row) }
     }
+    /// # Safety
+    /// * Validity must be writable
+    /// * `row` must be in range
     pub unsafe fn set_row_validity(&self, row: u64, valid: bool) {
         ffi::duckdb_validity_set_row_validity(self.0, row, valid);
     }
