0c5e1acd7b04bb2d7ebcd587e2f3a8dcc928d87c
Rongcui Dong <rongcuid@outlook.com>
2023-09-09 11:10:55 -0400

    Data chunk

--- crates/quackdb-internal/src/data_chunks.rs
+++ crates/quackdb-internal/src/data_chunks.rs
@@ -6,8 +6,15 @@ use crate::{ffi, vector::VectorHandle};
 pub struct DataChunkHandle(ffi::duckdb_data_chunk);
 
 impl DataChunkHandle {
+    /// # Safety
+    /// Takes ownership
     pub unsafe fn from_raw(raw: ffi::duckdb_data_chunk) -> Arc<Self> {
         Arc::new(Self(raw))
+    }
+    /// # Safety
+    /// Does not consider usage. Normally, let Rust automatically manage this.
+    pub unsafe fn destroy(&mut self) {
+        ffi::duckdb_destroy_data_chunk(&mut self.0);
     }
     pub fn reset(&self) {
         unsafe {
@@ -17,6 +24,8 @@ impl DataChunkHandle {
     pub fn column_count(&self) -> u64 {
         unsafe { ffi::duckdb_data_chunk_get_column_count(self.0) }
     }
+    /// # Safety
+    /// `col_idx` must be within valid range
     pub unsafe fn vector(&self, col_idx: u64) -> Arc<VectorHandle> {
         let v = ffi::duckdb_data_chunk_get_vector(self.0, col_idx);
         VectorHandle::from_raw(v)
@@ -24,7 +33,9 @@ impl DataChunkHandle {
     pub fn size(&self) -> u64 {
         unsafe { ffi::duckdb_data_chunk_get_size(self.0) }
     }
-    pub unsafe fn set_size_unchecked(&self, size: u64) {
+    /// # Safety
+    /// Caller ensures size is valid
+    pub unsafe fn set_size(&self, size: u64) {
         ffi::duckdb_data_chunk_set_size(self.0, size)
     }
 }
@@ -39,8 +50,6 @@ impl Deref for DataChunkHandle {
 
 impl Drop for DataChunkHandle {
     fn drop(&mut self) {
-        unsafe {
-            ffi::duckdb_destroy_data_chunk(&mut self.0);
-        }
+        unsafe { self.destroy() }
     }
 }
